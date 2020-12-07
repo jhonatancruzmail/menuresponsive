36fb9397e53a4402b5351b02563698cfee538871
jhonatanc <jhonatancruzmail@gmail.com>
2020-12-07 13:12:52 -0500

    actualizacion de menu subitems algunas rutas estaban mal

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,23 @@
 $(document).ready(main);
+
 var menuOculto = 1;
-function main(){
-    $('.menu-bar').click(function(){
-        if (menuOculto == 1){
+
+function main() {
+    $('.menu-bar').click(function() {
+        if (menuOculto == 1) {
             $('nav').animate ({
-                left:'0'
+                left: '0'
             });
-            menuOculto=0;
-        }
-        else{
-            $('nav').animate ({
-                left:'-100%'
-            });
-            menuOculto=1;
-        }
+            menuOculto = 0;
+         }
+         else {
+             $('nav').animate({
+                 left: '-100%'
+             });
+             menuOculto = 1;
+         }
     });
-$('.subMenu').click(function(){
-$(this).children('.children').slideToggle();
-})
+    $('.subMenu').click(function() {
+        $(this).children('.children').slideToggle();
+    }); 
 }
